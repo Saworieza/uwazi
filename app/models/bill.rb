3ccdd85cce294880dfe8fb046aa0ddd234b8f930
Sawo <saworiieza@gmail.com>
2016-05-17 13:31:09 +0000

    dyill on pdf

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,5 +1,10 @@
 class Bill < ActiveRecord::Base
-	has_attached_file :document, styles: {thumbnail: "60x60#"}
+	require 'mini_magick'
+pdf = Magick::ImageList.new("doc.pdf")
+thumb = pdf.scale(300, 300)
+thumb.write "doc.png"
+	
+	has_attached_file :document, styles: {:thumbnail => ["200x160#", :png]}
     validates_attachment :document, content_type: { :content_type => ["image/jpg", "image/jpeg", "image/png", "application/pdf"]}
 	
 	
